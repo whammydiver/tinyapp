5438fc6dc520eea0d4f8321a7c2ae2b84278919b
Paul Taylor <paul@whammydiver.com>
2022-06-28 16:22:24 -0400

    create urls_index.ejs

--- express_server.js
+++ express_server.js
@@ -5,22 +5,27 @@ const PORT = 8080;
 app.set('view engine', 'ejs');
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  'b2xVn2': 'http://www.lighthouselabs.ca',
+  '9sm5xK': 'http://www.google.com'
 };
 
 app.get('/', (req, res) => {
-  res.send("Hello!");
+  res.send('Hello!');
 });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-app.get("/urls.json", (req, res) => {
+app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
+app.get('/hello', (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get('/urls', (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars);
 });

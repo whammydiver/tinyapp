ec2c6f5fdbf5c3ff61e06eea1a1b902efca0242b
Paul Taylor <paul@whammydiver.com>
2022-07-01 14:06:54 -0400

    change URL database structure

--- express_server.js
+++ express_server.js
@@ -12,13 +12,19 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 
 let urlDatabase = {
-  'b2xVn2': 'http://www.lighthouselabs.ca',
-  '9sm5xK': 'http://www.google.com'
+  'b2xVn2': {
+    longURL: 'http://www.lighthouselabs.ca',
+    userID: "aJ48lW"
+  },
+  '9sm5xK': {
+    longURL: 'http://www.google.com',
+    userID: "aJ48lW"
+  }
 };
 
 const users = { 
-  "userRandomID": {
-    id: "userRandomID", 
+  "aJ48lW": {
+    id: "aJ48lW", 
     userID: "user@example.com", 
     password: "purple-monkey-dinosaur"
   },
